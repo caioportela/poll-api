71ad7b369e904178c71b77425d6b63359f2b5033
caioportela <caio_fabiio@outlook.com>
2020-07-15 01:46:57 -0300

    Change application and database init

--- api/__init__.py
+++ api/__init__.py
@@ -1,19 +1,26 @@
 from flask import Flask
 
-from api.database import db_session, init_db_command
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
 
     app = Flask(__name__, instance_relative_config=True)
-
-    app.cli.add_command(init_db_command)
+    app.config.from_mapping(DATABASE=SQLALCHEMY_DATABASE_URL)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
+
+    # Register the database commands
+    from api.database import init_app
+    init_app(app)
+
+    # Apply the blueprints to the app
+    from api.controllers import poll_controller
+    app.register_blueprint(poll_controller.bp)
 
     return app
--- api/database.py
+++ api/database.py
@@ -1,21 +1,54 @@
 import click
+from flask import current_app, g
 from flask.cli import with_appcontext
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-
 Base = declarative_base()
-Base.query = db_session.query_property()
+
+def get_db():
+    """Connect to the application's configured database."""
+
+    if 'db_session' not in g:
+        engine = create_engine(current_app.config['DATABASE'], connect_args={'check_same_thread': False})
+        db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+        Base.query = db_session.query_property()
+
+        g.engine = engine
+        g.db_session = db_session
+
+    return g.db_session
+
+def init_db():
+    """Clear existing data and create new tables."""
+
+    get_db()
+
+    from api.models import poll, poll_option
+
+    Base.metadata.drop_all(bind=g.engine)
+    Base.metadata.create_all(bind=g.engine)
+
+def close_db(e=None):
+    """Shutdown the session."""
+
+    db_session = g.pop('db_session', None)
+
+    if db_session is not None:
+        db_session.remove()
 
 @click.command('init_db')
 @with_appcontext
 def init_db_command():
-    """Initialize database."""
+    """Clear existing data and create new tables."""
 
-    from api.models import poll, poll_option
-    Base.metadata.create_all(bind=engine)
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    """Register database functions with the Flask app."""
+
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
