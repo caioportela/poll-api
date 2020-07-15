678b3a2a872659d62c37fca83ed8fd78edd291fa
caioportela <caio_fabiio@outlook.com>
2020-07-14 21:16:34 -0300

    Setup api

---
+++ api/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from api.database import db_session, init_db_command
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.cli.add_command(init_db_command)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    return app
